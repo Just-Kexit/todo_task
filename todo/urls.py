b09c9828f204dc80d9dbf052654759ed6ea2627b
Bohdan Mykyta <justkexitmail@gmail.com>
2026-02-23 14:58:38 +0200

    Add tag view and template, update sidebar

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from todo.views import HomeListView
+from todo.views import HomeListView, TagListView
 
 app_name = "todo"
 
 urlpatterns = [
     path("", HomeListView.as_view(), name="home_list"),
+    path("tags/", TagListView.as_view(), name="tag_list"),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.views import generic
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class HomeListView(generic.ListView):
     model = Task
     template_name = "todo/home_list.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
