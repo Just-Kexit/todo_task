ff9accb9ef083f07227932bda054dcc9f86bdae9
Bohdan Mykyta <justkexitmail@gmail.com>
2026-02-23 14:50:39 +0200

    Register tags in admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     list_filter = ("is_completed",)
+
+admin.site.register(Tag)
