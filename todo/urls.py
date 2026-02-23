4cc940086492bf37e642def81f045f9798e2f85d
Bohdan Mykyta <justkexitmail@gmail.com>
2026-02-23 14:23:50 +0200

    Update view, and fix urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,3 @@
-from operator import index
-
 from django.urls import path
 from todo.views import HomeListView
 
--- todo/views.py
+++ todo/views.py
@@ -5,3 +5,4 @@ from todo.models import Task
 
 class HomeListView(generic.ListView):
     model = Task
+    template_name = "todo/home_list.html"
